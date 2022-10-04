4effcf59020b23dcb525da23cf5ec064f0ee26be
Ahmed Aboalia <ahmed.aboalia2015@gmail.com>
2022-10-04 13:44:15 +0200

    Solve Tow Sum Problem

---
+++ Arrays/TwoSum/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+int[] TwoSum()
+{
+    int[] nums = new[] { 2, 7, 11, 15 };
+    int target = 9;
+
+    int arrayLength = nums.Length;
+    if (nums == null || arrayLength < 2)
+    {
+        return Array.Empty<int>();
+    }
+    for (int i = 0; i < arrayLength; i++)
+    {
+        for (int j = i + 1; j < arrayLength; j++)
+        {
+            if (nums[i] + nums[j] == target) return new int[] {
+                i,
+                j
+            };
+        }
+    }
+    return Array.Empty<int>();
+}
+
+Console.WriteLine(TwoSum());
